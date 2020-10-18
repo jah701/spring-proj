26d2eb4b161fb5e615ba480a72520d135e125d46
Jah <moskovienkor@gmail.com>
2020-10-18 18:17:20 +0300

    spring-proj-intro: - User class fixed (unique email annotation added, @table(name = "users") annotation added;

--- src/main/java/spring/intro/model/User.java
+++ src/main/java/spring/intro/model/User.java
@@ -1,16 +1,15 @@
 package spring.intro.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
+    @Column(unique = true)
     private String email;
     private String password;
 
